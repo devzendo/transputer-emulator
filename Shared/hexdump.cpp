1fcac0a3990b06b05b58afd01067122230777d8f
Matt Gumbley <matt.gumbley@devzendo.org>
2018-06-28 08:44:02 +0100

    Small formatting fixes.

--- Shared/hexdump.cpp
+++ Shared/hexdump.cpp
@@ -27,24 +27,24 @@ WORD32 offset=0;
 WORD32 left=len;
 WORD32 upto16, x;
 BYTE b;
-	while (left>0) {
-		for (i=0; i<78; i++) {
-			line[i]=' ';
+	while (left > 0) {
+		for (i = 0; i < 78; i++) {
+			line[i] = ' ';
 		}
-		line[9]=line[59]='|';
-		line[77]='\0';
+		line[9] = line[59] = '|';
+		line[77] = '\0';
 		sprintf(line, "%08X", offset);
-		line[8]=' ';
-		upto16=(left>16) ? 16 : left;
-		for (x=0; x<upto16; x++) {
-			b=buf[offset+x];
-			line[11+(3*x)]=hexdigs[(b&0xf0)>>4];
-			line[12+(3*x)]=hexdigs[b&0x0f];
-			line[61+x]=isprint((char)b) ? ((char)b) : '.';
+		line[8] = ' ';
+		upto16 = (left > 16) ? 16 : left;
+		for (x = 0; x < upto16; x++) {
+			b = buf[offset + x];
+			line[11 + (3 * x)] = hexdigs[(b & 0xf0) >> 4];
+			line[12 + (3 * x)] = hexdigs[b & 0x0f];
+			line[61 + x] = isprint((char)b) ? ((char)b) : '.';
 		}
 		logDebug(line);
-		offset+=16;
-		left-=16;
+		offset += 16;
+		left -= 16;
 	}
 }
 
