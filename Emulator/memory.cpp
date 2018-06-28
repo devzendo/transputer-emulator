e8d4a2067f5b0e1e95fa65224cca98acc73333ab
Matt Gumbley <matt.gumbley@devzendo.org>
2018-06-28 08:42:39 +0100

    Small formatting fixes.

--- Emulator/memory.cpp
+++ Emulator/memory.cpp
@@ -149,7 +149,7 @@ WORD32 Memory::getWord(WORD32 addr) {
 		// Irrespective of the emulator hosts's endianness, words are
 		// always stored in memory in little-endian form, as on a real
 		// Transputer. LSB first MSB last
-		w = (b[3]<<24) | (b[2]<<16) | (b[1]<<8) | b[0];
+		w = (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
 		if ((flags & DebugFlags_MemAccessDebugLevel) != MemAccessDebug_No) {
 			logDebugF("R 4 [%08X]=%08X", addr, w);
 		}
@@ -224,15 +224,15 @@ void Memory::blockCopy(WORD32 len, WORD32 srcAddr, WORD32 destAddr) {
 	WORD32 addr;
 	// TODO check this algo...
 	// How long will the read take?
-	for (i=0, j=wordsInBlock(len, srcAddr); i<j; i++) {
-		addr = srcAddr + (i<<2);
+	for (i = 0, j = wordsInBlock(len, srcAddr); i < j; i++) {
+		addr = srcAddr + (i << 2);
 		if (addr >= InternalMemStart && addr <= myMemEnd) {
 			myCurrentCycles += 1;
 		}
 	}
 	// How long will the write take?
-	for (i=0, j=wordsInBlock(len, destAddr); i<j; i++) {
-		addr = destAddr + (i<<2);
+	for (i = 0, j = wordsInBlock(len, destAddr); i < j; i++) {
+		addr = destAddr + (i << 2);
 		if (addr >= InternalMemStart && addr <= myMemEnd) {
 			myCurrentCycles += 1;
 		}
@@ -241,7 +241,7 @@ void Memory::blockCopy(WORD32 len, WORD32 srcAddr, WORD32 destAddr) {
 	// TODO optimise for best native performance later
 	WORD32 sA, dA;
 	BYTE b;
-	for (i=0, sA=srcAddr, dA=destAddr; i<len; i++, sA++, dA++) {
+	for (i = 0, sA = srcAddr, dA = destAddr; i < len; i++, sA++, dA++) {
 		// Read from source...
 		if (sA >= InternalMemStart && sA <= myMemEnd) {
 			if (sA > myHighestAccess) {
@@ -294,30 +294,30 @@ WORD32 offset=addr;
 WORD32 left=len;
 WORD32 upto16, x;
 BYTE b;
-	while (left>0) {
-		for (i=0; i<78; i++) {
-			line[i]=' ';
+	while (left > 0) {
+		for (i = 0; i < 78; i++) {
+			line[i] = ' ';
 		}
-		line[9]=line[59]='|';
-		line[77]='\0';
+		line[9] = line[59]='|';
+		line[77] = '\0';
 		sprintf(line, "%08X", offset);
-		line[8]=' ';
-		upto16=(left>16) ? 16 : left;
-		for (x=0; x<upto16; x++) {
+		line[8] = ' ';
+		upto16 = (left > 16) ? 16 : left;
+		for (x = 0; x < upto16; x++) {
 			if (isLegalMemory(offset + x)) {
 				b=myMemory[offset + x - InternalMemStart];
-				line[11+(3*x)]=hexdigs[(b&0xf0)>>4];
-				line[12+(3*x)]=hexdigs[b&0x0f];
-				line[61+x]=isprint((char)b) ? ((char)b) : '.';
+				line[11 + (3 * x)] = hexdigs[(b & 0xf0) >> 4];
+				line[12 + (3 * x)] = hexdigs[b & 0x0f];
+				line[61 + x] = isprint((char)b) ? ((char)b) : '.';
 			} else {
-				line[11+(3*x)]='-';
-				line[12+(3*x)]='-';
-				line[61+x]='-';
+				line[11 + (3 * x)] = '-';
+				line[12 + (3 * x)] = '-';
+				line[61 + x] = '-';
 			}
 		}
 		logInfo(line);
-		offset+=16;
-		left-=16;
+		offset += 16;
+		left -= 16;
 	}
 }
 
@@ -333,33 +333,33 @@ WORD32 w;
 01234567890123456789012345678901234567890123456789012345678901234567890123456789
 00000000 | 01234567 01234567 01234567 01234567 | .... .... .... ....
  */
-	while (left>0) {
-		for (i=0; i<78; i++) {
-			line[i]=' ';
+	while (left > 0) {
+		for (i = 0; i < 78; i++) {
+			line[i] = ' ';
 		}
-		line[9]=line[47]='|';
-		line[68]='\0';
+		line[9] = line[47] = '|';
+		line[68] = '\0';
 		sprintf(line, "%08X", offset);
-		line[8]=' ';
-		upto4=(left>4) ? 4 : left;
-		for (x=0; x<upto4; x++) {
+		line[8] = ' ';
+		upto4 = (left > 4) ? 4 : left;
+		for (x = 0; x < upto4; x++) {
 			if (isLegalMemory(offset + (x << 2))) {
 				b = myMemory + ((x << 2) + offset - InternalMemStart);
-				w = (b[3]<<24) | (b[2]<<16) | (b[1]<<8) | b[0];
-				sprintf(line+11+(9*x), "%08X", w);
-				line[49+(x*5)]=isprint((char)b[0]) ? ((char)b[0]) : '.';
-				line[50+(x*5)]=isprint((char)b[1]) ? ((char)b[1]) : '.';
-				line[51+(x*5)]=isprint((char)b[2]) ? ((char)b[2]) : '.';
-				line[52+(x*5)]=isprint((char)b[3]) ? ((char)b[3]) : '.';
+				w = (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
+				sprintf(line + 11 + (9 * x), "%08X", w);
+				line[49 + (x * 5)] = isprint((char)b[0]) ? ((char)b[0]) : '.';
+				line[50 + (x * 5)] = isprint((char)b[1]) ? ((char)b[1]) : '.';
+				line[51 + (x * 5)] = isprint((char)b[2]) ? ((char)b[2]) : '.';
+				line[52 + (x * 5)] = isprint((char)b[3]) ? ((char)b[3]) : '.';
 			} else {
-				sprintf(line+11+(9*x), "--------");
-				sprintf(line+11+(3*x), "---- ");
+				sprintf(line + 11 + (9 * x), "--------");
+				sprintf(line + 11 + (3 * x), "---- ");
 			}
-			line[19+(9*x)]=' ';
+			line[19 + (9 * x)] = ' ';
 		}
 		logInfo(line);
-		offset+=16;
-		left-=16;
+		offset += 16;
+		left -= 16;
 	}
 }
 
