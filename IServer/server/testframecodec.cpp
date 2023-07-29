49535a3bc387de243b230e2ad0cff592abf9e207
Matt Gumbley <matt.gumbley@gmail.com>
2023-07-29 20:35:33 +0100

    Update (C) year.

--- IServer/server/testframecodec.cpp
+++ IServer/server/testframecodec.cpp
@@ -5,7 +5,7 @@
 // License     : Apache License v2.0 - see LICENSE.txt for more details
 // Created     : 15/11/2019
 //
-// (C) 2005-2020 Matt J. Gumbley
+// (C) 2005-2023 Matt J. Gumbley
 // matt.gumbley@devzendo.org
 // http://devzendo.github.io/parachute
 //
--- src/main/resources/version.cpp
+++ src/main/resources/version.cpp
@@ -5,7 +5,7 @@
 // License     : Apache License v2.0 - see LICENSE.txt for more details
 // Created     : 18/01/2019
 //
-// (C) 2005-2020 Matt J. Gumbley
+// (C) 2005-2023 Matt J. Gumbley
 // matt.gumbley@devzendo.org
 // http://devzendo.github.io/parachute
 //
