1e4b5b89ff34d9bf74a4165aef580c7a3845febb
Matt Gumbley <matt.gumbley@devzendo.org>
2024-08-24 22:18:51 +0100

    Add another characterisation test for text / line end handling on Windows.

--- Shared/testcharacterisation.cpp
+++ Shared/testcharacterisation.cpp
@@ -21,12 +21,13 @@
 
 class TestCharacterisation : public TestTempFiles, public ::testing::Test {
 public:
-    std::string writeHelloWorldWithNewlines() {
+    std::string writeHelloWorldWithParticularLineEndings(const char* toWrite) {
         std::string testFileName = createRandomTempFileName();
         std::string testFilePath = pathJoin(tempdir(), testFileName);
         std::ios::openmode iosOpenMode = (std::ios_base::out | std::ios_base::trunc);
+        // Note: not std::ios_base::binary so it's opened in text mode.
         std::fstream fs(testFilePath, iosOpenMode);
-        fs << "hello\nworld\n";
+        fs << toWrite;
         fs.flush();
         fs.close();
 
@@ -36,9 +37,14 @@ public:
 
 
 #if defined(PLATFORM_WINDOWS)
-TEST_F(TestCharacterisation, CharacteriseCPlusPlusTextHandlingOnWindows)
+TEST_F(TestCharacterisation, CharacteriseCPlusPlusTextHandlingOnWindowsLFtoCRLF)
 {
-    std::string testFilePath = writeHelloWorldWithNewlines();
+    std::string testFilePath = writeHelloWorldWithParticularLineEndings("hello\nworld\n");
+    EXPECT_EQ(readFileContents(testFilePath), "hello\r\nworld\r\n");
+}
+TEST_F(TestCharacterisation, CharacteriseCPlusPlusTextHandlingOnWindowsCRLFtoCRLF)
+{
+    std::string testFilePath = writeHelloWorldWithParticularLineEndings("hello\r\nworld\r\n");
     EXPECT_EQ(readFileContents(testFilePath), "hello\r\nworld\r\n");
 }
 #endif
@@ -46,7 +52,7 @@ TEST_F(TestCharacterisation, CharacteriseCPlusPlusTextHandlingOnWindows)
 #if defined(PLATFORM_OSX) || defined(PLATFORM_LINUX)
 TEST_F(TestCharacterisation, CharacteriseCPlusPlusTextHandlingOnNonWindows)
 {
-    std::string testFilePath = writeHelloWorldWithNewlines();
+    std::string testFilePath = writeHelloWorldWithParticularLineEndings("hello\nworld\n");
     EXPECT_EQ(readFileContents(testFilePath), "hello\nworld\n");
 }
 #endif
