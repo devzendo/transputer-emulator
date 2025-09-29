ffedc35e01e37af56d4b6d07806fe8bf5be904a7
Matt Gumbley <matt.gumbley@devzendo.org>
2025-09-29 13:37:12 +0100

    The DataAckSender can now send an ack, that can be traced for verification in a test. Add a cross-wired TxRxPin for use in tests.

--- Shared/asynclink.h
+++ Shared/asynclink.h
@@ -15,11 +15,33 @@
 #ifndef _ASYNCLINK_H
 #define _ASYNCLINK_H
 
-#include <exception>
-
 #include "types.h"
 #include "link.h"
+#include "log.h"
 
+/*
+ * Using oversampling: The clock pulse triggers 16 times the bit frequency, giving 16 samples per bit.
+ * |         1111111|
+ * |1234567890123456|
+ *        xxx
+ * Only samples 7, 8 and 9 are read, and a majority vote taken to determine the actual value of the
+ * transmitted bit.
+ */
+
+/* Lowest level abstraction: TxRxPin, represents a pair of abstract pins. GPIOTxRxPin would use Pi Pico
+ * GPIO pins. Tests would use a CrosswiredTxRxPinPair, which gives a pair of TxRxPins, A and B,
+ * where setting A's Tx pin enables B's Rx pin. Setting B's Tx enables A's Rx. An AsyncLink
+ * would take a TxRxPin, and tests would create two AsyncLinks with the two TxRxPins back-to-back.
+ */
+class TxRxPin {
+public:
+    virtual ~TxRxPin() {}
+    virtual bool getRx() = 0;
+    virtual void setTx(bool state) = 0;
+};
+
+/* Highest level abstraction: AsyncLink, a state machine that uses the DataAckSender/Receiver
+ */
 class AsyncLink : public Link {
 public:
     AsyncLink(int linkNo, bool isServer);
@@ -33,5 +55,75 @@ public:
 private:
     WORD32 myWriteSequence, myReadSequence;
 };
+
+/* Medium level abstraction: DataAckSender/Receiver, a state machine that uses the Tx half of
+ * a TxRxPin to clock out an Ack or Data frame and can be queried for its state. DataAckReceiver,
+ * a state machine that senses the Rx half of a TxRxPin to clock in any received Ack and/or Data frame.
+ */
+
+// TODO move this implementation to the .cpp
+
+enum DataAckSenderState { IDLE, SENDING };
+
+class DataAckSender {
+public:
+    DataAckSender(TxRxPin& tx_rx_pin) : m_pin(tx_rx_pin) {
+        // TODO mutex {
+        m_state = IDLE;
+        // TODO }
+    }
+
+    DataAckSenderState state() {
+        // TODO mutex {
+        return m_state;
+        // TODO }
+    }
+
+    void sendAck() {
+        // TODO if m_state != IDLE throw up
+        // TODO mutex {
+        m_sampleCount = 0;
+        m_bits = 2;
+        m_data = 0x01;
+        m_state = SENDING;
+        // TODO }
+    }
+
+    void clock() {
+        //logDebugF("clock > %d sample count %d bits %d data 0x%04X", m_state, m_sampleCount, m_bits, m_data);
+        // TODO mutex {
+        switch (m_state) {
+            case IDLE:
+                break;
+            case SENDING:
+                const bool one = m_data & 0x0001;
+                m_pin.setTx(one);
+                m_sampleCount ++;
+                if (m_sampleCount == 16) {
+                    m_sampleCount = 0;
+                    m_bits--;
+                    m_data >>= 1;
+                    if (m_bits == 0) {
+                        m_state = IDLE;
+                    }
+                }
+                break;
+        }
+        // TODO }
+        //logDebugF("clock < %d sample count %d bits %d data 0x%04X", m_state, m_sampleCount, m_bits, m_data);
+    }
+
+    int _queueLength() {
+        return m_bits;
+    }
+
+private:
+    TxRxPin & m_pin;
+    DataAckSenderState m_state;
+    int m_sampleCount;
+    int m_bits;
+    WORD16 m_data;
+};
+
 
 #endif // _ASYNCLINK_H
--- Shared/testasynclink.cpp
+++ Shared/testasynclink.cpp
@@ -11,10 +11,11 @@
 //
 //------------------------------------------------------------------------------
 
+#include <vector>
+
 #include "gtest/gtest.h"
 #include "link.h"
 #include "asynclink.h"
-#include "linkfactory.h"
 #include "log.h"
 
 class AsyncLinkTest : public ::testing::Test {
@@ -60,6 +61,7 @@ protected:
     AsyncLink *linkB = nullptr;
 };
 
+/*
 TEST_F(AsyncLinkTest, WriteAndReadByte) {
     linkA->writeByte(16);
     EXPECT_EQ(linkB->readByte(), 16);
@@ -79,3 +81,145 @@ TEST_F(AsyncLinkTest, WriteAndReadBytes) {
     EXPECT_EQ(readBuf[2], 0x60);
     EXPECT_EQ(readBuf[3], 0x21);
 }
+*/
+
+class CrosswiredTxRxPinPair {
+    bool aPin = false;
+    bool bPin = false;
+
+    class CrosswiredPin: TxRxPin {
+    public:
+        CrosswiredPin(const char *side, bool *rxstate, bool *txstate) : m_rxstate(rxstate), m_txstate(txstate) {
+           m_side = *side;
+        }
+
+        ~CrosswiredPin() override {}
+
+        bool getRx() override {
+            bool retval = (bool) *m_rxstate;
+            logDebugF("RX %c pin state is %d", m_side, retval);
+            return retval;
+        }
+
+        void setTx(const bool state) override {
+            logDebugF("Setting TX %c pin state to %d", m_side, state);
+            *m_txstate = state;
+        }
+    private:
+        bool *m_rxstate;
+        bool *m_txstate;
+        char m_side = ' ';
+    };
+
+    CrosswiredPin m_aPinEnd;
+    CrosswiredPin m_bPinEnd;
+public:
+    CrosswiredTxRxPinPair() :
+        m_aPinEnd(CrosswiredPin("A", &aPin, &bPin)),
+        m_bPinEnd(CrosswiredPin("B", &bPin, &aPin)) {
+    }
+
+    TxRxPin & pairA() { return reinterpret_cast<TxRxPin&>(m_aPinEnd); };
+
+    TxRxPin & pairB() { return reinterpret_cast<TxRxPin&>(m_bPinEnd); };
+
+    ~CrosswiredTxRxPinPair() {
+    }
+};
+
+TEST_F(AsyncLinkTest, CrosswiredTxRxPinPairExecise) {
+    CrosswiredTxRxPinPair pair;
+    TxRxPin & pairA = pair.pairA();
+    TxRxPin & pairB = pair.pairB();
+
+    // Initial state
+    logInfo("Checking initial state");
+    EXPECT_EQ(pairA.getRx(), false);
+    EXPECT_EQ(pairB.getRx(), false);
+
+    logInfo("Starting pin mutations");
+    // Set A, see it at B
+    pairA.setTx(true);
+    EXPECT_EQ(pairB.getRx(), true);
+    // No change in A
+    EXPECT_EQ(pairA.getRx(), false);
+
+    // Set B, see it at A
+    pairB.setTx(true);
+    EXPECT_EQ(pairA.getRx(), true);
+    // No change in B
+    EXPECT_EQ(pairB.getRx(), true);
+
+    // Clear A, see it at B
+    pairA.setTx(false);
+    EXPECT_EQ(pairB.getRx(), false);
+    // No change in A
+    EXPECT_EQ(pairA.getRx(), true);
+
+    // Clear B, see it at A
+    pairB.setTx(false);
+    EXPECT_EQ(pairA.getRx(), false);
+    // No change in B
+    EXPECT_EQ(pairB.getRx(), false);
+}
+
+class ClockingPinTracer {
+public:
+    ClockingPinTracer(TxRxPin& tx_rx_pin) : m_pin(tx_rx_pin) {
+    }
+
+    void clock() {
+        m_trace.push_back(m_pin.getRx());
+    }
+
+    std::vector<bool> heard() {
+        return m_trace;
+    }
+
+private:
+    TxRxPin & m_pin;
+    std::vector<bool> m_trace;
+};
+
+std::vector<bool> generate_sample_vector_from_bits(const int *bits, const int len) {
+    std::vector<bool> out;
+    logDebugF("generating sample vector from %d bits", len);
+    for (int i=0; i<len; i++) {
+        const bool one = (bits[i] == 1);
+        logDebugF("expecting %d", one);
+        for (int b=0; b<16; b++) {
+            out.push_back(one);
+        }
+    }
+    return out;
+}
+
+TEST_F(AsyncLinkTest, AckCanBeSent) {
+    CrosswiredTxRxPinPair pair;
+    TxRxPin & pairA = pair.pairA();
+    TxRxPin & pairB = pair.pairB();
+    ClockingPinTracer trace(pairB);
+    DataAckSender sender(pairA);
+    EXPECT_EQ(sender.state(), IDLE);
+
+    sender.sendAck(); // Needs clocking to send the signals out. 16 clock pulses/bit.
+
+    // An ack is a 1 followed by a 0.
+    EXPECT_EQ(sender.state(), SENDING);
+    EXPECT_EQ(sender._queueLength(), 2);
+    for (int i=0; i<32; i++) {
+        EXPECT_EQ(sender.state(), SENDING);
+
+        sender.clock();
+        trace.clock();
+    }
+    EXPECT_EQ(sender.state(), IDLE);
+    EXPECT_EQ(sender._queueLength(), 0);
+    const std::vector<bool> heard = trace.heard();
+    EXPECT_EQ(heard.size(), 32);
+    const int expected_ack_bits[2] = { 1, 0 };
+    const std::vector<bool> expected = generate_sample_vector_from_bits(expected_ack_bits, 2);
+    EXPECT_EQ(heard, expected);
+}
+
+// TODO how to signal you can't ack or send data in a non-idle state
